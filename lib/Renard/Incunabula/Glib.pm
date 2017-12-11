7113d4b4cd6428267538926ca592a9495458755f
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-12-11 01:09:46 -0600

    Initial commit

---
+++ lib/Renard/Incunabula/Glib.pm
@@ -0,0 +1,38 @@
+use Modern::Perl;
+package Renard::Incunabula::Glib;
+# ABSTRACT: Helper for using Glib
+
+use strict;
+use warnings;
+
+use Glib;
+
+=method Inline
+
+  use Inline C with => qw(Renard::Incunabula::Glib);
+
+Returns the flags needed to configure L<Inline::C> for using the
+L<Glib> XS API.
+
+=cut
+sub Inline  {
+	return unless $_[-1] eq 'C';
+
+	require ExtUtils::Depends;
+	my $ref = ExtUtils::Depends::load('Glib');
+
+	my $config = +{ map { uc($_) => $ref->{$_} } qw(inc libs typemaps) };
+
+	# Set CCFLAGS to the value of INC directly. This is to get around some
+	# shell parsing / quoting bug that causes INC to quote parts that
+	# should not be quoted.
+	$config->{CCFLAGS} = delete $config->{INC};
+
+	$config->{AUTO_INCLUDE} = <<C;
+#include <gperl.h>
+C
+
+	$config;
+}
+
+1;
